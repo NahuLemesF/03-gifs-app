e1c9691199c82a05971da4f7096b5f2342d1eb94
Nahuel Lemes <nahulem@gmail.com>
2025-12-27 09:24:39 -0300

    test: add test to get-gifs-by-query file

---
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -0,0 +1,61 @@
+import { beforeEach, describe, expect, test } from 'vitest';
+import AxiosMockAdapter from 'axios-mock-adapter';
+
+import { getGifsByQuery } from './get-gifs-by-query.action';
+import { giphyApi } from '../api/giphy.api';
+import { giphyResponseMock } from '../../../tests/mocks/giphy.response.data';
+
+describe('getGifsByQuery', () => {
+  
+  const mock = new AxiosMockAdapter(giphyApi);
+  beforeEach(() => {
+    mock.reset()
+  })
+  // test('should return a list of gifs', async () => {
+  //   const gifs = await getGifsByQuery('goku');
+  //   const [gif1] = gifs;
+
+  //   expect(gif1).toEqual({
+  //     id: expect.any(String),
+  //     height: expect.any(Number),
+  //     title: expect.any(String),
+  //     url: expect.any(String),
+  //     width: expect.any(Number),
+  //   });
+  // });
+
+  test('should return a list of gifs', async () => {
+    mock.onGet('/search').reply(200, giphyResponseMock);
+
+    const gifs = await getGifsByQuery('goku');
+
+    expect(gifs.length).toBe(10);
+
+    gifs.forEach((gif) => {
+      expect(typeof gif.id).toBe('string');
+      expect(typeof gif.title).toBe('string');
+      expect(typeof gif.url).toBe('string');
+      expect(typeof gif.width).toBe('number');
+      expect(typeof gif.height).toBe('number');
+    });
+  });
+
+  test('should return an empty list of gifs if query is empty', async () => {
+
+    const gifs = await getGifsByQuery('');
+
+    expect(gifs.length).toBe(0);
+  });
+
+  test('should handle error when API returns an error', async () => { 
+
+    mock.onGet('/search').reply(400, {
+      data: 'Bad request. 400 Error'
+    })
+
+    const gifs = await getGifsByQuery('goku');
+
+    expect(gifs.length).toBe(0);
+
+   })
+});
--- src/gifs/api/giphy.api.test.ts
+++ src/gifs/api/giphy.api.test.ts
@@ -5,10 +5,12 @@ describe('giphyApi', () => {
   test('should be configured correctly', () => {
     const params = giphyApi.defaults.params;
 
+    //toBe para evaluar parametros individualmente.
     expect(giphyApi.defaults.baseURL).toBe('https://api.giphy.com/v1/gifs');
     expect(params.lang).toBe('es');
     expect(params.api_key).toBe(import.meta.env.VITE_GIPHY_API_KEY);
 
+    //toEqual o toStrictEqual para evaluar objetos.
     expect(params).toStrictEqual({
       lang: 'es',
       api_key: import.meta.env.VITE_GIPHY_API_KEY,
