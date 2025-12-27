fc984c3f0a3f5d85af0c464df9af3459f9ee20e0
Nahuel Lemes <nahulem@gmail.com>
2025-12-27 10:14:37 -0300

    test: add spyOn to get-gifs-by-query test

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import AxiosMockAdapter from 'axios-mock-adapter';
 
 import { getGifsByQuery } from './get-gifs-by-query.action';
@@ -6,11 +6,10 @@ import { giphyApi } from '../api/giphy.api';
 import { giphyResponseMock } from '../../../tests/mocks/giphy.response.data';
 
 describe('getGifsByQuery', () => {
-  
   const mock = new AxiosMockAdapter(giphyApi);
   beforeEach(() => {
-    mock.reset()
-  })
+    mock.reset();
+  });
   // test('should return a list of gifs', async () => {
   //   const gifs = await getGifsByQuery('goku');
   //   const [gif1] = gifs;
@@ -41,21 +40,26 @@ describe('getGifsByQuery', () => {
   });
 
   test('should return an empty list of gifs if query is empty', async () => {
-
     const gifs = await getGifsByQuery('');
 
     expect(gifs.length).toBe(0);
   });
 
-  test('should handle error when API returns an error', async () => { 
+  test('should handle error when API returns an error', async () => {
+    const consoleErrorSpy = vi
+    .spyOn(console, 'error')
+    .mockImplementation(() => {})
 
     mock.onGet('/search').reply(400, {
-      data: 'Bad request. 400 Error'
-    })
+      data: 'Bad request. 400 Error',
+    });
 
     const gifs = await getGifsByQuery('goku');
 
     expect(gifs.length).toBe(0);
+    expect(consoleErrorSpy).toHaveBeenCalled();
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+    expect(consoleErrorSpy).toHaveBeenCalledWith(expect.anything());
 
-   })
+  });
 });
